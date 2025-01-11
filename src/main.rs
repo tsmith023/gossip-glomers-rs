f81b3b61b808140948fb176f86568e52962a3c9e
Tommy Smith <tommy@weaviate.io>
2025-01-11 17:37:35 +0000

    Initial commit of echo get started challenge

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use async_trait::async_trait;
+use maelstrom::{protocol::Message, Node, Result, Runtime, done};
+use std::sync::Arc;
+
+#[derive(Clone, Default)]
+struct EchoHandler {}
+
+#[async_trait]
+impl Node for EchoHandler {
+    async fn process(&self, runtime: Runtime, req: Message) -> Result<()> {
+        if req.get_type() == "echo" {
+            let echo = req.body.clone().with_type("echo_ok");
+            return runtime.reply(req, echo).await;
+        }
+        done(runtime, req)
+    }
+}
+
+pub(crate) fn main() -> Result<()> {
+    Runtime::init(try_main())
+}
+
+async fn try_main() -> Result<()> {
+    let handler = Arc::new(EchoHandler::default());
+    Runtime::new().with_handler(handler).run().await
+}
